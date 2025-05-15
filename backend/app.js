cbc78f0026b8c703ed0cc75afbfa7f10a939d417
Aboubakary Abdourahman <aboubakayabdourahman@gmail.com>
2025-05-15 12:21:14 +0200

    cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+app.use(cors({
+  origin: 'http://localhost:8080', // autorise le front Vue.js
+  credentials: true                // si tu veux envoyer des cookies ou des headers spéciaux
+}));
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
